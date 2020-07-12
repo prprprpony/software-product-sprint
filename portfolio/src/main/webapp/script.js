81e0e1123a66444d8d838bbde05cba5c4bcf1d72
Ji-Gui Zhang <3254tg89@gmail.com>
2020-07-12 17:06:22 +0800

    Update portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,16 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ["Pop Pop!", "Leonard likes this post.", "I can't believe I spent ten bucks on this! I don't know any of these people!", "My name is Alex!",
+      "We're the only species on earth that observes shark week.", "TV's the best dad there is. TV never came home drunk, TV never forgot me at the zoo, TV never abused and insulted me... unless you count Cop Rock.",
+      "The funny thing about being smart, is that you can get through most of life without ever having to do any work.",
+      "I see your value now.",
+      "Our first assignment is a documentary. The're like real movies, but with ugly people.",
+      "I looked inside Nicolas Cage and I found a secret: People are random and pointless.",
+      "I painted a tunnel on the side of the library. When it dries, I'm going for it.",
+      "Do you understand what a conspiracy is? When you conspire with everyone you come across, you're not really conspiring with anyone. You're just doing random crap.",
+      "It's not a pen, it's a principle!",
+    ];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
